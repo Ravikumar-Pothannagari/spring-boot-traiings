7a763708cf42f85aeb68a6e5f7d86c9437e39400
Ravikumar Pothannagari <kumar.sku85@gmail.com>
2019-11-21 17:30:42 +0530

    Zuul API Gateway Server

---
+++ zuul-api-gateway-server/src/main/java/com/ibm/zuulapigatewayserver/filters/ZuualAPIGatewayFilter.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.ibm.zuulapigatewayserver.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * @author RavikumarPothannagar
+ *
+ */
+@Component
+public class ZuualAPIGatewayFilter extends ZuulFilter {
+	 
+	  @Override
+	  public String filterType() {
+	    return "pre";
+	  }
+	 
+	  @Override
+	  public int filterOrder() {
+	    return 1;
+	  }
+	 
+	  @Override
+	  public boolean shouldFilter() {
+	    return true;
+	  }
+	 
+	  @Override
+	  public Object run() {
+	    RequestContext ctx = RequestContext.getCurrentContext();
+	    HttpServletRequest request = ctx.getRequest();
+	 
+	    System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+	    return null;
+	  }
+
+}
